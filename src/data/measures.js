7aec2a2bcb0b786ef21c01a362fc10d889f96467
Daniel Koban <danielkoban@gmail.com>
2019-05-01 19:01:08 +0200

    add data

---
+++ src/data/measures.js
@@ -0,0 +1,131 @@
+export const measuresInEnglish = [
+    {
+        id: 1,
+        usMeasureSymbol: "°F",
+        usMeasureName: "Fahrenheit scale",
+        euMeasureSymbol: "°C",
+        usMeasureName: "Celsius scale"
+    },
+    {
+        id: 2,
+        usMeasureSymbol: "lb",
+        usMeasureName: "pound",
+        euMeasureSymbol: "kg",
+        usMeasureName: "kilogram"
+    },
+    {
+        id: 3,
+        usMeasureSymbol: "oz",
+        usMeasureName: "ounce",
+        euMeasureSymbol: "g",
+        usMeasureName: "gram"
+    },
+    {
+        id: 4,
+        usMeasureSymbol: "mi",
+        usMeasureName: "mile",
+        euMeasureSymbol: "km",
+        usMeasureName: "kilometer"
+    },
+    {
+        id: 5,
+        usMeasureSymbol: "yd",
+        usMeasureName: "yard",
+        euMeasureSymbol: "m",
+        usMeasureName: "meter"
+    },
+    {
+        id: 6,
+        usMeasureSymbol: "ft",
+        usMeasureName: "foot",
+        euMeasureSymbol: "cm",
+        usMeasureName: "centimeter"
+    },
+    {
+        id: 7,
+        usMeasureSymbol: "in",
+        usMeasureName: "inch",
+        euMeasureSymbol: "mm",
+        usMeasureName: "millimeter"
+    },
+    {
+        id: 8,
+        usMeasureSymbol: "gal",
+        usMeasureName: "US gallon",
+        euMeasureSymbol: "l",
+        usMeasureName: "liter"
+    },
+    {
+        id: 9,
+        usMeasureSymbol: "pint",
+        usMeasureName: "US liquid pint",
+        euMeasureSymbol: "ml",
+        usMeasureName: "milliliter"
+    }
+]
+
+export const measuresInPolish = [
+    {
+        id: 1,
+        usMeasureSymbol: "°F",
+        usMeasureName: "stopnie Fahrenheita",
+        euMeasureSymbol: "°C",
+        usMeasureName: "stopnie celsjusza"
+    },
+    {
+        id: 2,
+        usMeasureSymbol: "lb",
+        usMeasureName: "funt",
+        euMeasureSymbol: "kg",
+        usMeasureName: "kilogram"
+    },
+    {
+        id: 3,
+        usMeasureSymbol: "oz",
+        usMeasureName: "uncja",
+        euMeasureSymbol: "g",
+        usMeasureName: "gram"
+    },
+    {
+        id: 4,
+        usMeasureSymbol: "mi",
+        usMeasureName: "mila",
+        euMeasureSymbol: "km",
+        usMeasureName: "kilometer"
+    },
+    {
+        id: 5,
+        usMeasureSymbol: "yd",
+        usMeasureName: "jard",
+        euMeasureSymbol: "m",
+        usMeasureName: "metr"
+    },
+    {
+        id: 6,
+        usMeasureSymbol: "ft",
+        usMeasureName: "stopa",
+        euMeasureSymbol: "cm",
+        usMeasureName: "centymeter"
+    },
+    {
+        id: 7,
+        usMeasureSymbol: "in",
+        usMeasureName: "cal",
+        euMeasureSymbol: "mm",
+        usMeasureName: "milimeter"
+    },
+    {
+        id: 8,
+        usMeasureSymbol: "gal",
+        usMeasureName: "galon am.",
+        euMeasureSymbol: "l",
+        usMeasureName: "litr"
+    },
+    {
+        id: 9,
+        usMeasureSymbol: "pint",
+        usMeasureName: "pinta płynna am.",
+        euMeasureSymbol: "ml",
+        usMeasureName: "mililiter"
+    },
+]
